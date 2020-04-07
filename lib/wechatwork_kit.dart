bb6c5070b37c2d50bc3484ee97d248a9980cce5c
obzone <shaoyecheng@live.cn>
2020-04-07 23:02:32 +0800

    添加iOS配置注释

--- lib/wechatwork_kit.dart
+++ lib/wechatwork_kit.dart
@@ -26,6 +26,12 @@ class WechatworkKit {
     return _channel.invokeMethod('SSO');
   }
 
+/*! @brief 检查企业微信是否已被用户安装
+ *
+ * @return 企业微信已安装返回YES，未安装返回NO。
+ * @note 由于iOS系统的限制，在iOS9及以上系统检测企业微信是否安装，需要将企业微信的scheme"wxwork"(云端版本)及"wxworklocal"(本地部署版本)添加到工程的Info.plist中的LSApplicationQueriesSchemes白名单里，否则此方法总是会返回NO。
+ * 详情参考 https://developer.apple.com/documentation/uikit/uiapplication/1622952-canopenurl
+ */
   static Future isWWAppInstalled() {
     return _channel.invokeMethod('isWWAppInstalled');
   }
