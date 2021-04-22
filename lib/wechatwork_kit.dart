18d6ef2bc0ac2370e8b4517aa6f8a3c606cf602c
shaoyecheng <shaoyecheng@pagoda.com.cn>
2021-04-22 14:42:42 +0800

    migrating to null safety

--- lib/wechatwork_kit.dart
+++ lib/wechatwork_kit.dart
@@ -1,18 +1,24 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class WechatworkKit {
   static const MethodChannel _channel = const MethodChannel('wechatwork_kit');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static Future registerApp({@required String schema, @required String corpId, @required String agentId}) {
-    return _channel.invokeMethod('registerApp', <String, dynamic>{'schema': schema, 'corpId': corpId, 'agentId': agentId});
+  static Future registerApp(
+      {required String schema,
+      required String corpId,
+      required String agentId}) {
+    return _channel.invokeMethod('registerApp', <String, dynamic>{
+      'schema': schema,
+      'corpId': corpId,
+      'agentId': agentId
+    });
   }
 
   static Future sso() {
